df32b3f9a561df450b478cc7577fca852b97be01
Thomas Theunen <thomas.theunen@forward.eu>
2022-10-27 17:25:59 +0200

    Add docs for controllers

--- cartridges/plugin_turnstile/cartridge/controllers/Account.js
+++ cartridges/plugin_turnstile/cartridge/controllers/Account.js
@@ -1,11 +1,48 @@
 'use strict';
 
 var server = require('server');
+
 var turnstile = require('*/cartridge/scripts/middleware/turnstile');
 
 server.extend(module.superModule);
 
+/**
+ * Account-Login : Add Turnstile token validation. (Ajax)
+ * @name Turnstile/Account-Login
+ * @function
+ * @memberof Account
+ * @param {middleware} - turnstile.validateAjax
+ * @param {querystringparameter} - rurl - redirect url. The value of this is a number. This number then gets mapped to an endpoint set up in oAuthRenentryRedirectEndpoints.js
+ * @param {httpparameter} - loginEmail - The email associated with the shopper's account.
+ * @param {httpparameter} - loginPassword - The shopper's password
+ * @param {httpparameter} - loginRememberMe - Whether or not the customer has decided to utilize the remember me feature.
+ * @param {httpparameter} - csrf_token - a CSRF token
+ * @param {category} - sensitive
+ * @param {returns} - json
+ * @param {serverfunction} - post
+ */
 server.prepend('Login', turnstile.validateAjax);
+
+/**
+ * Account-SubmitRegistration : Add Turnstile token validation. (Ajax)
+ * @name Turnstile/Account-SubmitRegistration
+ * @function
+ * @memberof Account
+ * @param {middleware} - turnstile.validateAjax
+ * @param {querystringparameter} - rurl - redirect url. The value of this is a number. This number then gets mapped to an endpoint set up in oAuthRenentryRedirectEndpoints.js
+ * @param {httpparameter} - dwfrm_profile_customer_firstname - Input field for the shoppers's first name
+ * @param {httpparameter} - dwfrm_profile_customer_lastname - Input field for the shopper's last name
+ * @param {httpparameter} - dwfrm_profile_customer_phone - Input field for the shopper's phone number
+ * @param {httpparameter} - dwfrm_profile_customer_email - Input field for the shopper's email address
+ * @param {httpparameter} - dwfrm_profile_customer_emailconfirm - Input field for the shopper's email address
+ * @param {httpparameter} - dwfrm_profile_login_password - Input field for the shopper's password
+ * @param {httpparameter} - dwfrm_profile_login_passwordconfirm: - Input field for the shopper's password to confirm
+ * @param {httpparameter} - dwfrm_profile_customer_addtoemaillist - Checkbox for whether or not a shopper wants to be added to the mailing list
+ * @param {httpparameter} - csrf_token - hidden input field CSRF token
+ * @param {category} - sensitive
+ * @param {returns} - json
+ * @param {serverfunction} - post
+ */
 server.prepend('SubmitRegistration', turnstile.validateAjax);
 
 module.exports = server.exports();
--- cartridges/plugin_turnstile/cartridge/controllers/Login.js
+++ cartridges/plugin_turnstile/cartridge/controllers/Login.js
@@ -4,6 +4,17 @@ var server = require('server');
 
 server.extend(module.superModule);
 
+/**
+ * Login-Show: Add the Site Key to the login page.
+ * @name Turnstile/Login-Show
+ * @function
+ * @memberof Login
+ * @param {querystringparameter} - rurl - Redirect URL
+ * @param {querystringparameter} - action - Action on submit of Login Form
+ * @param {category} - sensitive
+ * @param {renders} - isml
+ * @param {serverfunction} - get
+ */
 server.prepend('Show', function (req, res, next) {
     var Site = require('dw/system/Site');
     var currentSite = Site.getCurrent();
